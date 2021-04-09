873711ab661faf0842baa64f80cfedc2cc769b75
Jaclyn French <jaclynmariefrench@gmail.com>
2021-04-09 14:16:03 -0500

    Did this work?

---
+++ scripts/database.js
@@ -0,0 +1,50 @@
+/*
+ *  To get you started, here's some properties of Bart.
+ *  You need to add more properties to complete his
+ *  representation as an object. Then add all the other
+ *  fish to the collection.
+ */
+const database = {
+    fish: [
+        {
+            name: "Sponge Bob",
+            species: "Sponge",
+            length: 3,
+            food: "Krabby Patty",
+            location: "Bikini Bottom"
+            
+        }
+
+        {
+            name: "Patrick Star",
+            species: "Starfish",
+            length: 4,
+            food: "Everything",
+            location: "Bikini Bottom",
+        }
+
+        {
+            name: "Random Fish Guy",
+            species: "Fish",
+            length: 5,
+            food: "Taco",
+            location: "Bikini Bottom",
+        }
+
+        {
+            name: "Squidward",
+            species: "Squid",
+            length: 5,
+            food: "Anything Fancy",
+            location: "Bikini Bottom",
+        }
+        
+        {
+            name: "Plankton",
+            species: "Plankton",
+            length: 2,
+            food: "Krabby Patty Secret Formula",
+            location: "Bikini Bottom",
+        }
+    ]
+}
